615467c8745be33dd26ef74525fb1013d535c107
jsyishan <404678173@qq.com>
2016-01-29 15:09:04 +0800

    UnityPros

---
+++ test/Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+//using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof (CharacterMotion))]
+
+public class CharacterControl : MonoBehaviour {
+
+    private CharacterMotion m_Character;
+    private bool m_Jump;
+    private bool m_Spell;
+    private PlayerInput pi;
+
+	void Awake() {
+        m_Character = GetComponent<CharacterMotion>();
+        pi = GetComponent<PlayerInput>();
+    }
+	
+	
+	void Update () {
+	    if(!m_Jump) {
+            //  m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+            m_Jump = pi.isJumping();
+        }
+
+        if(!m_Spell) {
+            m_Spell = pi.isSpelling();
+        }
+	}
+
+    void FixedUpdate() {
+        // float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        float h = pi.getMove();
+        m_Character.Move(h,m_Jump);
+       // m_Character.Spell(m_Spell);
+        m_Jump = false;
+    }
+}
---
+++ test/Assets/Scripts/CharacterMotion.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class CharacterMotion : MonoBehaviour {
+
+    //基本动作相关属性定义
+    [SerializeField]private float m_MaxSpeed = 10f;
+    [SerializeField]private float m_JumpForce = 400f;
+    [SerializeField]private LayerMask m_WhatIsGround;
+
+    //其他属性/控件
+    private Transform m_GroundCheck;
+    const float k_GroundedRadius = .2f;
+    private bool m_Grounded;
+    private bool m_Spelling;
+
+    private Transform m_CeilingCheck;
+    const float k_CeilingRadius = .1f;
+
+    private Animator m_Anim;
+   // private AnimationState m_AnimState;
+    private Rigidbody2D m_Rigidbody;
+    private bool m_FacingRight = true;
+
+
+    void Awake() {
+        m_GroundCheck = transform.Find("GroundCheck");
+        m_CeilingCheck = transform.Find("CeilingCheck");
+        m_Anim = GetComponent<Animator>();
+       // m_AnimState = a
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate() {
+        m_Grounded = false;
+        m_Spelling = false;
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position,k_GroundedRadius,m_WhatIsGround);
+        for(int i = 0;i < colliders.Length;i++) {
+            if(colliders[i].gameObject != gameObject) {
+                m_Grounded = true;
+            }
+        }
+        m_Anim.SetBool("Ground",m_Grounded);
+
+        m_Anim.SetFloat("vSpeed",m_Rigidbody.velocity.y);
+    }
+
+    
+    public void Spell(bool spell) {
+        if(spell && !m_Anim.GetBool("Spell") ) {
+            m_Anim.SetBool("Spell",true);
+        }
+        else if(!spell && m_Anim.GetBool("Spell")) {
+            m_Anim.SetBool("Spell",false);
+        }
+    }
+    
+
+    public void Move(float move,bool jump) {
+        
+            m_Anim.SetFloat("Speed",Mathf.Abs(move));
+            m_Rigidbody.velocity = new Vector2(move * m_MaxSpeed,m_Rigidbody.velocity.y);
+
+            if(move > 0 && !m_FacingRight) {
+                Flip();
+            }
+
+            else if(move < 0 && m_FacingRight) {
+                Flip();
+            }
+
+        if(jump && m_Grounded && m_Anim.GetBool("Ground")) {
+            m_Grounded = false;
+            m_Anim.SetBool("Ground",false);
+            m_Rigidbody.AddForce(new Vector2(0f,m_JumpForce));
+        }
+
+    }
+
+    private void Flip() {
+        m_FacingRight = !m_FacingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    public float getMaxSpeed() {
+        return m_MaxSpeed;
+    }
+}
